08905475822b8d1749b76e25bfd15c694092f3a3
nsih6571 <57556313+nsih6571@users.noreply.github.com>
2021-11-01 23:58:03 +0900

    11.1
    init2

--- JSim/Assets/Script/UserController.cs
+++ JSim/Assets/Script/UserController.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class UserController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5;
+    public float r_speed = 60;
+
+    public float power;
+
+
+
+    int _gear = 0;
+
     void Start()
     {
-        
+        power = 10;
+    }
+ 
+    void FixedUpdate()
+    {
+        Move();
+        Rotate();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Move()
     {
-        
+        float _moveDirZ = Input.GetAxisRaw("Vertical");
+        Vector3 _moveVertical = transform.forward * _moveDirZ;
+
+        Vector3 _velocity = (_moveVertical).normalized * 10;
+
+        this.gameObject.GetComponent<Rigidbody>().MovePosition(transform.position + _velocity * Time.deltaTime);
+    }
+
+    void Rotate()
+    {
+        if(Input.GetKey("a"))
+        {
+            this.transform.Rotate(Vector3.down, Time.deltaTime * r_speed);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            this.transform.Rotate(Vector3.up, Time.deltaTime * r_speed);
+        }
+
+
     }
 }
